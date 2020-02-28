4ab4d54bc2425d8280c6d5fc8ac89666e23195ba
Gajendra95 <gajendra.ranjith@gmail.com>
2020-02-28 12:00:04 +0530

    1st commit

---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading;
+
+namespace PAIS
+{
+    [TestFixture]
+    public class SRN
+    {
+        [Test]
+        public void SR()
+        {
+            string Url = "http://www.google.com/";
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(Url);
+
+
+            //driver.FindElement(By.Id("fakebox-input")).SendKeys("selenium");
+            Task.Delay(2000).Wait();
+            driver.Close();
+        }
+    }
+}
